fbf877903ced7e127ffe4b42e0d2f83565872949
buihao090320-sudo <buihao090320@gmail.com>
2026-03-26 07:43:10 +0000

    xoahet/tuan24

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('.top-bar');
-    if (window.scrollY > 50) {
-        header.style.padding = '5px 0'; // Thu nhỏ khi cuộn
-    } else {
-        header.style.padding = '15px 0';
-    }
-});
